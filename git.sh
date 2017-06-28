aee92b1c62c3eac44dec64eff946efbda6dba838
gezhuang0717 <zhuang.ge@riken.jp>
2017-06-28 18:32:17 +0900

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# ms2" >> README.md
+git init
+git add -A
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/gezhuang0717/ms2.git
+git push -u origin master
